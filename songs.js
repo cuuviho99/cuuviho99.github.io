94a6f23f3576fff074a1668d30e057596c2b87b5
HoangNguyen <lenny.kun99@gmail.com>
2025-10-30 21:04:43 +0700

    update

---
+++ songs.js
@@ -0,0 +1,423 @@
+window.SONGS = [
+  {
+    id: 100,
+    author: "Emily",
+    name: "Xin Anh Đừng",
+    path: "./assets/music/XinAnhDung.mp3",
+    image: "./assets/img/Emily.jpg"
+  },
+  {
+    id: 101,
+    name: "Có chắc yêu là đây",
+    author: "Sơn Tùng MTP",
+    path: "./assets/music/CoChacYeuLaDay-MTP.mp3",
+    image: "./assets/img/CoChacYeuLaDay.jpg"
+  },
+  {
+    id: 102,
+    name: "Cứ thở đi",
+    author: "Đức Phúc & JukySan",
+    path: "./assets/music/CuThoDi-DucPhuc_JukySan.mp3",
+    image: "./assets/img/CuThoDi.jpg"
+  },
+  {
+    id: 103,
+    name: "Floating With You",
+    author: "Venxento",
+    path: "./assets/music/FloatingWithYou-Venxento.mp3",
+    image: "./assets/img/FloatingWithYou.jpg"
+  },
+  {
+    id: 104,
+    name: "Lung Lay",
+    author: "Cucak",
+    path: "./assets/music/LungLay-CuCak.mp3",
+    image: "./assets/img/LungLay.jpg"
+  },
+  {
+    id: 105,
+    name: "Mê",
+    author: "Hoàng Duyên",
+    path: "./assets/music/Me-HoangDuyen.mp3",
+    image: "./assets/img/Me.jpg"
+  },
+  {
+    id: 106,
+    name: "Never Letting Go",
+    author: "Venxento",
+    path: "./assets/music/NeverLettingGo-Venxento.mp3",
+    image: "./assets/img/NeverLettingGo.jpg"
+  },
+  {
+    id: 107,
+    name: "Northern Lights",
+    author: "Venxento",
+    path: "./assets/music/NorthernLights-Venxento.mp3",
+    image: "./assets/img/Venxento.jpg"
+  },
+  {
+    id: 108,
+    name: "Ngày Đầu Tiên",
+    author: "Đức Phúc",
+    path: "./assets/music/NgayDauTien-DucPhuc.mp3",
+    image: "./assets/img/NgayDauTien.jpg"
+  },
+  {
+    id: 109,
+    name: "Yêu rồi sẽ biết",
+    author: "Cucak",
+    path: "./assets/music/YeuRoiSeBiet-CuCak.mp3",
+    image: "./assets/img/YeuRoiSeBiet.jpg"
+  },
+  {
+    id: 110,
+    name: "Tình Đầu",
+    author: "Tăng Duy Tân",
+    path: "./assets/music/TinhDau-TangDuyTan.mp3",
+    image: "https://photo-resize-zmp3.zmdcdn.me/w240_r1x1_jpeg/cover/0/a/d/1/0ad18644161b1bbe41bc1ca0471600ba.jpg"
+  },
+  {
+    id: 111,
+    name: "Tháng tư là lời nói dối của em",
+    author: "Hà Anh Tuấn",
+    path: "./assets/music/ThangTuLaLoiNoiDoiCuaEm-HaAnhTuan.mp3",
+    image: "https://thoidai.com.vn/stores/news_dataimages/anh.khuong/032019/31/11/2902_1476093493424_500.jpg"
+  },
+  {
+    id: 112,
+    name: "Âm thầm bên em (Lofi)",
+    author: "Sơn Tùng MTP",
+    path: "./assets/music/AmThamBenEm(Lofi)-SonTungMTP.mp3",
+    image: "https://i1.sndcdn.com/artworks-TTCQb0HV7lksG9GF-FxNJRw-t500x500.jpg"
+  },
+  {
+    id: 113,
+    name: "Nơi này có anh",
+    author: "Sơn Tùng MTP",
+    path: "./assets/music/NoiNayCoAnh-SonTungMTP.mp3",
+    image: "https://upload.wikimedia.org/wikipedia/vi/1/1d/N%C6%A1i_n%C3%A0y_c%C3%B3_anh_-_Single_Cover.jpg"
+  },
+  {
+    id: 114,
+    name: "Lạ Lùng",
+    author: "Vũ",
+    path: "./assets/music/LaLung-Vu.mp3",
+    image: "https://avatar-ex-swe.nixcdn.com/song/2018/01/26/1/8/9/0/1516930244148_640.jpg"
+  },
+  {
+    id: 115,
+    name: "Querry",
+    author: "QNT x TRUNG TRẦN ft RPT MCK (Prod. By RASTZ)",
+    path: "./assets/music/QUERRY-QNT x TRUNG TRẦN ft RPT MCK Prod By RASTZ.mp3",
+    image: "./assets/img/Querry.jpg"
+  },
+  {
+    id: 116,
+    name: "Mặt Mộc",
+    author: "Pham Nguyên Ngoc x VAnh x Ân Nhi",
+    path: "./assets/music/MĂT MÔC-Pham Nguyên Ngoc x VAnh x Ân Nhi.mp3",
+    image: "./assets/img/matmoc.jpg"
+  },
+  {
+    id: 117,
+    name: "Ngày Em Đi",
+    author: "Touliver X Lê Hiếu X Soobin Hoàng Sơn",
+    path: "./assets/music/NGÀY MAI EM ĐI 2017 TOULIVER X LÊ HIẾU X SOOBIN HOÀNG SƠN.mp3",
+    image: "./assets/img/NgayMaiEmDi.jpg"
+  },
+  {
+    id: 118,
+    author: "Lương Minh Trang",
+    name: "Hạnh Phúc Đó Em Không Có",
+    path: "./assets/music/HanhPhucDoEmKhongCo.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 119,
+    author: "Doãn Hiếu",
+    name: "Họ Yêu Ai Mất Rồi ",
+    path: "./assets/music/HoYeuAiMatRoi.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 120,
+    author: "Tino",
+    name: "Ừ Có Anh Đây",
+    path: "./assets/music/ucoanhday.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 121,
+    author: "Phát Hồ x JokeS Bii ft DinhLong",
+    name: "Cố Giang Tình (Orinn Remix)",
+    path: "./assets/music/CoGiangTinh.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 122,
+    author: "Phát Hồ x JokeS Bii",
+    name: "Họa Mây (DinhLong Remix)",
+    path: "./assets/music/hoamay.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 123,
+    author: "OBITO x BIG H",
+    name: "Thanh Âm Buồn Bã",
+    path: "./assets/music/thanhambuonba.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 124,
+    author: "Bảo Jen ft Tunny  DuyNh",
+    name: "Cố Nhân (Cover)",
+    path: "./assets/music/conhan.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 125,
+    author: " Mạc Khiếu Tỷ Tỷ",
+    name: "Kiếp Sau Không Chắc Sẽ Còn Gặp Được Anh",
+    path: "./assets/music/kiepsaukhongchacsecongapduocanh.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 126,
+    author: " Ngải Thần",
+    name: "Thời Không Sai Lệch (Remix)",
+    path: "./assets/music/thoikhongsailech.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 127,
+    author: "Tưởng Tuyết Nhi ",
+    name: "Yến Vô Hiết (Remix)",
+    path: "./assets/music/yenvohiet.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 128,
+    author: "Chi Pu",
+    name: "Em Sai Rồi Anh Xin Lỗi Em Đi",
+    path: "./assets/music/esraxled.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 129,
+    author: "B Ray",
+    name: "Ex's Hate Me",
+    path: "./assets/music/ex.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 130,
+    author: "Sơn Tùng MTP",
+    name: "Lạc Trôi (Triple D remix)",
+    path: "./assets/music/LacTroi.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 131,
+    name: "Anh đánh rơi người yêu này",
+    author: "AMEE & Andiliez",
+    path: "./assets/music/AnhDanhRoiNguoiYeuNay-Amee_Andiez.mp3",
+    image: "./assets/img/AnhDanhRoiNguoiYeuNay.png"
+  },
+  {
+    id: 132,
+    author: "Chipu",
+    name: "Anh Ơi Ở Lại",
+    path: "./assets/music/AnhOiOLai.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 133,
+    author: "Đạt G",
+    name: "Khó vẽ nụ cười",
+    path: "./assets/music/KhoVeNuCuoi.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 134,
+    author: "Dig Didzay",
+    name: "Nếu Anh Đi (Cover)",
+    path: "./assets/music/NeuAnhDi.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 135,
+    author: "Hà My",
+    name: "Anh Ta Bỏ Em Rồi - Cover",
+    path: "./assets/music/AnhTaBoEmRoi.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 136,
+    author: "Trịnh Đình Quang",
+    name: "Thất tình",
+    path: "./assets/music/ThatTinh.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 137,
+    author: "Thanh Goll",
+    name: "Cafe Đắng Và Mưa (Acoustic Cover)",
+    path: "./assets/music/CFDangVaMua.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 138,
+    author: "Minh Vương",
+    name: "Đừng làm anh đau",
+    path: "./assets/music/DungLamAnhDau.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 139,
+    author: "Thanh Goll",
+    name: "Cánh Chim Hải Âu (Acoustic Cover)",
+    path: "./assets/music/CanhChimHaiAu.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 140,
+    author: "Minh Vương",
+    name: "Nỗi đau xót xa",
+    path: "./assets/music/NoiDauXotXa.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 141,
+    author: "Minh Vương",
+    name: "Em ơi lên phố",
+    path: "./assets/music/EmOiLenPho.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 142,
+    author: "Thanh Goll",
+    name: "Đêm Trăng Tình Yêu (Acoustic Cover)",
+    path: "./assets/music/DemTrangTinhYeu.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 143,
+    author: "Hoàng Yến Chibi",
+    name: "Ngồi Bên Em",
+    path: "./assets/music/NgoiBenEm.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 144,
+    author: "B Ray",
+    name: "Cao Ốc 20",
+    path: "./assets/music/CaoOc20.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 145,
+    author: "HKT",
+    name: "Mặt Trái Của Sự Thật",
+    path: "./assets/music/MatTraiCuaSuThat.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 146,
+    author: "Thương Võ",
+    name: "Còn gì đau hơn chữ đã từng (Cover)",
+    path: "./assets/music/ConGiDauHonChuDaTung.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 147,
+    author: "Đinh Tùng Huy",
+    name: "Cứ Ngỡ Là Anh",
+    path: "./assets/music/CuNgoLaAnh.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 148,
+    author: "Quang Vinh, Bảo Thy ",
+    name: "Ngôi Nhà Hoa Hồng",
+    path: "./assets/music/NgoiNhaHoaHong.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 149,
+    author: "Thanh Hưng",
+    name: "Đúng Người Đúng Thời Điểm",
+    path: "./assets/music/DungNguoiDungThoiDiem.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 150,
+    author: "Hương Tràm",
+    name: "Em gái mưa",
+    path: "./assets/music/EmGaiMua.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 151,
+    author: "Lê Thúy Chi",
+    name: "Tình Yêu Mang Theo (Cover)",
+    path: "./assets/music/TinhYeuMangTheo.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 152,
+    author: "Hương Tràm",
+    name: "Duyên Mình Lỡ",
+    path: "./assets/music/DuyenMinhLo.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 153,
+    author: "Đức Phúc",
+    name: "Hết Thương Cạn Nhớ",
+    path: "./assets/music/HetThuongCanNho.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 154,
+    author: "Mr Siro",
+    name: "Một Bước Yêu Vạn Dặm Đau",
+    path: "./assets/music/MotBuocYeuVanDamDau.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 155,
+    author: "Hiền Hồ",
+    name: "Rồi Người Thương Cũng Hóa Người Dưng",
+    path: "./assets/music/RoiNguoiThuongCungHoaNguoiDung.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 156,
+    author: "Khánh Ngọc, Nhật Tinh Anh ",
+    name: "Vầng Trăng Khóc",
+    path: "./assets/music/VangTrangKhoc.mp3",
+    image: "./assets/img/def.jpg"
+  },
+  {
+    id: 157,
+    author: "Phạm Hồng Phước ",
+    name: "Khi Người Lớn Cô Đơn",
+    path: "./assets/music/KhiNguoiLonCoDon.mp3",
+    image: "./assets/img/def.jpg"
+  }
+];
+document.addEventListener('click', e => {
+  const item = e.target.closest('.playlist--song');
+  if (!item) return;
+  const audio = document.querySelector('#audio');
+  const index = Number(item.dataset.index);
+  const song = window.SONGS[index];
+  if (song) {
+    audio.src = song.path;
+    audio.play().catch(() => {}); // tránh lỗi autoplay trên mobile
+    const title = document.querySelector('.header h2');
+    const thumb = document.querySelector('.cd--thumb');
+    if (title) title.textContent = song.name;
+    if (thumb) thumb.style.backgroundImage = `url('${song.image}')`;
+  }
+});
